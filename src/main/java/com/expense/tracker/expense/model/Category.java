60cec88d73ad9f33aa502ba42b7c907b41a9789a
dev-rm <54745323+dev-rm@users.noreply.github.com>
2021-01-15 08:52:08 +0530

    Add category entity

--- src/main/java/com/expense/tracker/expense/model/Category.java
+++ src/main/java/com/expense/tracker/expense/model/Category.java
@@ -1,30 +1,25 @@
 package com.expense.tracker.expense.model;
 
-import java.util.Set;
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@NoArgsConstructor
 @Data
-@Table(name="user")
-public class User {
-
+@Table(name="category")
+public class Category {
+	
 	@Id
 	private Long id;
 	
 	private String name;
 	
-	private String email;
-	
-	@OneToMany
-	private Set<Category> category;
+	@ManyToOne(cascade=CascadeType.PERSIST)
+	private User user;
 }
