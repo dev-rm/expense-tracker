9aa9a6b32abeb773aee7de0295303c0513da8a6a
dev-rm <54745323+dev-rm@users.noreply.github.com>
2021-01-16 15:51:11 +0530

    Add data to tables

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,11 @@
+insert into user values (1,'John','johndoe@gmail.com')
+insert into user values (2,'Jane','janedoe@gmail.com')
+insert into user values (3,'Adam','adam@adam.com')
+
+insert into category values (1,'Travel')
+insert into category values (2,'Groceries')
+insert into category values (3,'Other')
+
+insert into expense values (100, 'Visiting Singapore','2020-06-16T17:00:00.000Z',1,1)
+insert into expense values (101, 'Buying oil','2020-06-15T17:00:00.000Z',2,2)
+insert into expense values (102, 'Buying medicines','2020-06-14T17:00:00.000Z',3,3)
