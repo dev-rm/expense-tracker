f0f63c1c2210afbba754f60860476d4ca0d2e41c
dev-rm <54745323+dev-rm@users.noreply.github.com>
2021-01-16 15:12:39 +0530

    Add expense entity

---
+++ src/main/java/com/expense/tracker/expense/model/Expense.java
@@ -0,0 +1,31 @@
+package com.expense.tracker.expense.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="expense")
+public class Expense {
+	
+	@Id
+	private Long id;
+	
+	private Instant expenseDate;
+	
+	private String descript;
+	
+	@ManyToOne
+	private Category category;
+	
+	@ManyToOne
+	private User user;
+}
