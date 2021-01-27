212b36c85b5ccbf2ad6bfe8c5689a62b4d026e80
dev-rm <54745323+dev-rm@users.noreply.github.com>
2021-01-27 07:27:47 +0530

    Add react components

--- expense-app/src/index.js
+++ expense-app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Category from './Category';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <Category/>
   </React.StrictMode>,
   document.getElementById('root')
 );
